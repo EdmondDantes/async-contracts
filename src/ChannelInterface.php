21eec3df848d74ed4b41135741bdad7b68f4f01f
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-22 18:55:05 +0300

    % refactoring ChannelInterface

--- src/ChannelInterface.php
+++ src/ChannelInterface.php
@@ -14,16 +14,6 @@ interface ChannelInterface extends ClosableInterface
     public function send(mixed $data): void;
 
     /**
-     * Send data to the channel and block until the data is received by the receiver or the timeout is reached.
-     */
-    public function sendAsync(mixed $data, CancellationInterface $cancellation = null): void;
-    
-    /**
-     * Send data to the channel and return a promise that will be resolved when the data is received by the receiver.
-     */
-    public function sendWithPromise(mixed $data, CancellationInterface $cancellation = null): FutureInterface;
-    
-    /**
      * Receive data from the channel and block until the data is sent by the sender.
      */
     public function receive(): mixed;
